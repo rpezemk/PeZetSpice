e2824e623602abe2bd17752f1ce8c1820964f558
rpezemk <rpezemk@gmail.com>
2026-01-29 22:47:31 +0100

    paper sketch + scaling

--- PeZetSpice/MainWindow.xaml.cs
+++ PeZetSpice/MainWindow.xaml.cs
@@ -20,5 +20,49 @@ namespace PeZetSpice
         {
             InitializeComponent();
         }
+        private double scale = 1.0;
+        private bool isPanning = false;
+        private Point origin = new Point();
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            scale = (e.Delta > 0 ? 1.1 : 0.9);
+            var mouse = e.GetPosition(MyGrid);
+            var matrix = MainPaperView.RenderTransform.Value;
+
+            matrix.ScaleAt(scale, scale, mouse.X, mouse.Y);
+            MainPaperView.RenderTransform = new MatrixTransform(matrix);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isPanning = true;
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isPanning = false;
+        }
+
+        private void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // isPanning = false;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(!Mouse.LeftButton.Equals(MouseButtonState.Pressed))
+                isPanning = false;
+            
+            if (isPanning)
+            {
+                var mousePos = e.GetPosition(MyGrid);
+                var offsetX = mousePos.X - origin.X;
+                var offsetY = mousePos.Y - origin.Y;
+                var matrix = MainPaperView.RenderTransform.Value;
+                matrix.Translate(offsetX, offsetY);
+                MainPaperView.RenderTransform = new MatrixTransform(matrix);
+            }
+            origin = e.GetPosition(MyGrid);
+        }
     }
 }
--- PeZetSpice/Views/PaperView.xaml.cs
+++ PeZetSpice/Views/PaperView.xaml.cs
@@ -24,5 +24,46 @@ namespace PeZetSpice.Views
         {
             InitializeComponent();
         }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var w = PaperBorder.ActualWidth;
+            var h = PaperBorder.ActualHeight;
+
+
+            var nx = (int)w / 50 + 1;
+            var ny = (int)h / 50 + 1;
+            var xs = Enumerable.Range(0, nx).Select(k => k * 50).ToList();
+            var ys = Enumerable.Range(0, ny).Select(k => k * 50).ToList();
+
+            foreach(var x in xs)
+            {
+                var tb = new TextBlock { Text = x.ToString(), Foreground = new SolidColorBrush(Colors.DarkGray), FontSize = 8 };
+                Canvas.SetLeft(tb, x);
+                TopCanvas.Children.Add(tb);
+            }
+
+            foreach (var x in xs)
+            {
+                var tb = new TextBlock { Text = x.ToString(), Foreground = new SolidColorBrush(Colors.DarkGray), FontSize = 8 };
+                Canvas.SetLeft(tb, x);
+                BottomCanvas.Children.Add(tb);
+            }
+
+            foreach (var y in ys)
+            {
+                var tb = new TextBlock { Text = y.ToString(), Foreground = new SolidColorBrush(Colors.DarkGray), FontSize = 8 };
+                Canvas.SetTop(tb, h - y);
+                LeftCanvas.Children.Add(tb);
+            }
+
+            foreach(var y in ys)
+            {
+                var tb = new TextBlock { Text = y.ToString(), Foreground = new SolidColorBrush(Colors.DarkGray), FontSize = 8 };
+                Canvas.SetTop(tb, h - y);
+                RightCanvas.Children.Add(tb);
+            }
+
+        }
     }
 }
